d00ab29260959972fb21c8aaf178565f708565d7
huryur <yuriyhurnyak@gmail.com>
2016-12-13 13:04:01 +0200

    Task 4 added

---
+++ task4/main.js
@@ -0,0 +1,21 @@
+var octothorpe = '#',
+    result = '',
+    i = 0,
+    j = 0;
+
+while (i < 7) { // Set how much lines do you need
+    if (i % 2 != 0) {
+        while (j < 10) { // Set how much octothorpes do you need
+            result += octothorpe + ' ';
+            j++;
+        }
+        console.log(result);
+    } else {
+        while (j < 10) { // Set how much octothorpes do you need
+            result += octothorpe + ' ';
+            j++;
+        }
+        console.log(' ' + result);
+    }
+    i++;
+}
