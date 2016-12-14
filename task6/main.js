3c4dff27231437f806072e306018b22b17a3d02a
huryur <yuriyhurnyak@gmail.com>
2016-12-14 10:34:40 +0200

    formatting fixed

--- task6/main.js
+++ task6/main.js
@@ -2,21 +2,18 @@ var guessNumber,
     userNumber,
     count = 0;
 
-guessNumber = Math.floor(Math.random()*100);
+guessNumber = Math.floor(Math.random() * 100);
 (function quessing() {
     userNumber = prompt('Type your number');
     count++;
-    if (userNumber > guessNumber){
+    if (userNumber > guessNumber) {
         alert('Your number is higher then mine, try again');
         quessing();
-    }
-    else if(userNumber < guessNumber){
+    } else if (userNumber < guessNumber) {
         alert('Your number is lower then mine, try again');
         quessing();
-    }
-    else {
+    } else {
         alert('You are right, the number is ' + guessNumber +
             ' It takes you ' + count + ' steps to guess the number!');
-
     }
 })();
