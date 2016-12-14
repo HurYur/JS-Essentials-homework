39a21b2b1cca8f5c04f73aab930eecdc494d694b
huryur <yuriyhurnyak@gmail.com>
2016-12-14 11:36:18 +0200

    console.log changed to alert

--- task2/main.js
+++ task2/main.js
@@ -5,4 +5,4 @@ var x1 = 55,
 	x2 = prompt('What is your x location coordinates?', 53);
 	y2 = prompt('What is your y location coordinates?', 42);
 	var distance = Math.sqrt( Math.pow(x2-x1 , 2) + Math.pow(y2-y1 , 2), 2);
-    console.log('Distance is: ' + distance);
+    alert('Distance is: ' + distance);
